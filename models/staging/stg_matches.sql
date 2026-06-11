8f7e0bbd2d1c68ed59e40742525dd134c2146a53
Özge EFE <a.ozgeefe@gmail.com>
2026-06-11 06:49:57 +0000

    Update stg_matches staging model

--- models/staging/stg_matches.sql
+++ models/staging/stg_matches.sql
@@ -1,2 +1,10 @@
-select *
+select
+    id as match_id,
+    gameid,
+    platformid,
+    queueid,
+    seasonid,
+    duration as game_duration_seconds,
+    creation as game_creation_ts,
+    version as game_version
 from `league-of-legend-498906.LOL.matches`
